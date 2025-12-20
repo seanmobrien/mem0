7d788082526e8a86e12c02a01c7251956bd6aa4c
sean <seanmobrien@hotmail.com>
2025-12-20 00:25:24 +0000

    chore: bugfix ACL policy

--- openmemory/keycloak/packages/kc-casefile-js-policy/kc-casefile-policy.js
+++ openmemory/keycloak/packages/kc-casefile-js-policy/kc-casefile-policy.js
@@ -1,6 +1,6 @@
-const debugMessage = function(/*message*/) {
+const debugMessage = function(message) {
   // Uncomment the line below to enable debug logging
-  // print('[case-file:acl] ' + message);
+  print('[case-file:acl] ' + message);
 };
 
 debugMessage('------------------------------------ [case-file:acl]: Begin ------------------------------------');
